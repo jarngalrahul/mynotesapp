2b959f845389d6aedb0b20deabcb25f921bd46c4
rahul-perxl <140041988+rahul-perxl@users.noreply.github.com>
2024-12-16 18:00:59 +0530

    step-18

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:mynotesapp/extensions/list/filter.dart';
 import 'package:mynotesapp/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
@@ -10,6 +11,7 @@ class NotesService {
   Database? _db;
 
   List<DatabaseNote> _notes = [];
+  DatabaseUser? _user;
 
   //Creating a singleton instance of noteservice so that it is not initialized repeatedly
   static final NotesService _shared = NotesService._sharedInstance();
@@ -18,21 +20,37 @@ class NotesService {
       onListen: () {
         _notesStreamController.sink.add(_notes);
       },
-      
     );
   }
 
   factory NotesService() => _shared;
-  late final StreamController<List<DatabaseNote>> _notesStreamController ;      
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadingAllNotes();
+        }
+      });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
